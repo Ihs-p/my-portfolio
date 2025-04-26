5a65639a33ba4e604e3291584c67a2764ea2aa26
ihsan <ihsanp3553@gmail.com>
2025-04-26 19:12:07 +0530

    fixed  alignments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import { RevealOnScroll } from "./RevealOnScroll";
 export const Footer = () => {
   return (
     <>
-      <section className="border-t border-gray-400 mx-60 py-5 text-sm text-gray-300 flex justify-between ">
+      <section className="border-t border-gray-400 mx-10 md:mx-30 lg:mx-60 py-5 md:py-5 text-sm text-gray-300  md:flex justify-between items-center md:justify-between ">
         
-        <div>
+        <div className="flex  justify-center md:justify-start pb-3">
           <span>
             ©
             {new Date().toLocaleDateString("en-US", {
@@ -17,11 +17,11 @@ export const Footer = () => {
           </span>
         </div>
 
-        <div className="flex items-center justify-between ">
-          <a href="https://github.com/Ihs-p" target="_blank" className="mx-10">
+        <div className="flex items-center justify-center ">
+          <a href="https://github.com/Ihs-p" target="_blank" className="mx-10 text-white">
             GitHub
           </a>
-          <a target="_blank" href="www.linkedin.com/in/ihsan-p">
+          <a target="_blank" href="www.linkedin.com/in/ihsan-p" className="text-white">
             Linked In
           </a>
         </div>
